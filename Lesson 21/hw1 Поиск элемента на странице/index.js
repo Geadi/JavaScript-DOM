c557ddc87624c091656f88be63d605f4b65008b2
Geadi <66213778+Geadi@users.noreply.github.com>
2020-06-12 12:51:15 +0300

    add hw1

---
+++ Lesson 21/hw1 Поиск элемента на странице/index.js
@@ -0,0 +1,13 @@
+const getTitleElement = () => {
+    let titleElem = document.querySelector('.title');
+    console.dir(titleElem);
+    return titleElem;
+}
+getTitleElement();
+
+const getInputElement = () => {
+    let inputElem = document.querySelector('input[type = "text"]');
+    console.dir(inputElem);
+    return inputElem;
+};
+getInputElement();
